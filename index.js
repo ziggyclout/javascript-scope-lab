6cb84ee9bd5e6caf0c7e38f5052c462fcdfb31aa
ziggyclout <idakiallan989@gmail.com>
2025-12-06 05:28:47 +0300

    Added addBurger function with function-scoped variable

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+// Global scope
+let burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+// Function scope
+function addBurger() {
+    let newBurger = 'Flatburger';
+    burgers.push(newBurger);
+}
+
+//Block scope
+if (true) {
+    let anotherNewBurger = 'Maple Bacon Burger';
+    burgers.push(anotherNewBurger);
+
+    function changeFeaturedDrink() {
+        featuredDrink = 'The JavaShake';
+    }
+}
